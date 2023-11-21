ff8ecdc4809069dbfb6e78787dc33bf38871b02e
lifewawj <arevaloaj01@gmail.com>
2023-11-21 02:23:40 -0800

    Dayjs Current Date Display Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = dayjs();
+
+$('#current_date').text(today.format('MMM D, YYYY'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +25,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
