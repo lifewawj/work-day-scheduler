be6166df44f8623321d9324f77f01f814837111a
lifewawj <arevaloaj01@gmail.com>
2023-11-21 17:10:18 -0800

    Date Display Fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var today = dayjs();
-
-$('#current_date').text(today.format('MMM D, YYYY'));
+console.log(dayjs())
 
 
+var today = dayjs()
 
+$('#current_date').text(today.format('dddd' + ',' + ' ' + 'MMMM' + ' ' + 'DD'))
+
+
+// save button function
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
